ccf776f96fd0f6acf5b9b47f16927cd061396a0d
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-06-01 11:05:47 -0600

    Initial commit

--- SimpleSimon Says/ViewController.swift
+++ SimpleSimon Says/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var roundCount: Int = 1
+    var challengeArray: [Int] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+ 
+    
+    
+    @IBAction func startButton(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        roundCount = 1
+        challengeArray = []
+        
+    }
+    
+    @IBAction func buttonOne(_ sender: Any) {
+        print("1")
+    }
+    
+    @IBAction func buttonTwo(_ sender: Any) {
+        print("2")
+    }
+    
+    @IBAction func buttonThree(_ sender: Any) {
+        print("3")
+    }
+    
+    @IBAction func buttonFour(_ sender: Any) {
+        print("4")
+    }
+    
 
-
+    @IBOutlet weak var roundCounter: UILabel!
+    
 }
 
